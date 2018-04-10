6dbec14240d7b6cf02f06b6781cfa63ac47fcbed
und3rmysk1n <undermyskin@inbox.ru>
2018-04-10 13:32:17 +0300

    Добавил примеров для работы с объектами.

--- 5_javascript/js/objects.js
+++ 5_javascript/js/objects.js
@@ -1,34 +1,49 @@
 console.log('----------- Массивы -----------');
-var ninjas = ['Leo', 'Mike', 'Raph', 'Don'];
-console.log(ninjas[0]);
-console.log(ninjas[6]);
 
-var arrayOfSix = new Array(6);
-console.log('First element of arrayOfSix: ', arrayOfSix[0]);
-console.log('Length: ', arrayOfSix.length);
+var simpleArray = [];
+var ninjas = ['Leo', 'Mike', 'Raph', 'Don'];
+
+console.log('Length: ', ninjas.length);
+
 
 var matrix= [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+console.log(matrix[0]);
 console.log('Left center number of matrix: ', matrix[1][0]);
+
+for (var i = 0; i < ninjas.length; i++) {
+    console.log('My name is: ', ninjas[i]);
+}
 
 var stringAsArray = 'Строка - тоже массив';
 console.log('Third letter: ', stringAsArray[2]);
 console.log('String length: ', stringAsArray.length);
 
+
+
 console.log('----------- Объекты -----------');
 // Объекты
+
+var emptyObject = {};
+
 var mouse = {
     name: 'Mickey',
-    age: 89,
+    age: 90,
+    pet: {
+        name: 'Pluto'
+    },
     sayHello: function() {
-        console.log('Hello, I am Mickey!');
+        console.log('My name is Mickey!');
     }
 };
-mouse.sayHello();
+console.log('Age is: ', mouse['age']);
+console.log(mouse);
 
+console.log('Mickeys pet: ', mouse.pet.name);
 
 // Объекты и их свойства
 var Jamie = {
@@ -45,9 +60,16 @@ Jamie.sayHello();
 var User = function(name, age) {
     this.name = name;
     this.age = age;
+    this.toString = function () {
+        return this.name + ' and i am ' + this.age + ' years old!';;
+    }
 }
 
 var John = new User('John', '32');
 var Therion = new User('Therion', '27');
 console.log(John);
 console.log(Therion);
+
+
+console.log('I am ' + John);
+console.log('I am ' + Therion);
